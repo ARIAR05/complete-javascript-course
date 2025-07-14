2747b807f901d7517ed091568528b76e35e7e023
ARIAR05 <132704389+ARIAR05@users.noreply.github.com>
2025-07-14 03:09:08 +0000

    archivos modificados, fundamentals de javascript

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,32 @@
+
+// const person = {
+//   firstName: 'Jonas',
+//   lastName: 'Schmedtmann',
+//   age: 2037 - 1991,
+//   job: 'teacher',
+//   friends: ['Michael', 2, 'michaela', 'Han']
+// };
+
+// console.log("hola")
+// // console.log(person.firstName + ' has ' + person.friends.length + ' friends, his bestfriend is ' + person.friends[0]);//
+
+// const types = [];
+
+// for (let i = 0; i < person.friends.length; i++) {
+
+//   console.log(person.friends[i]);
+//   // types[i] = typeof person.friends[i]; 
+//   types.push(typeof person.friends[i]);
+// }
+
+// console.log(types);
+// for (let i=0;i<=4;i++){
+//   console.log(`INICIANDO EL SET ${i}`);
+//   for(let r=0; r<=4; r++){
+//     console.log(`   Vamoos repeticion ${r}, demole`)
+
+//   }
+// }
+
+let dice = Math.trunc(Math.random()*10);
+dice !== 6 ? console.log(dice) : console.log('no 6');
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,38 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 23;
+const y = "l";
+
+console.log("holaaay");
+addEventListener("DOMContentLoaded", (e) => {
+  const title = document.querySelector("h1");
+  title.style.color = "red";
+});
+
+const array1 = [4, 6, 7, 8, 1, -3];
+const array2 = [24, 66, 9, 6, -4];
+
+const array3 = array1.concat(array2);
+
+function min(array) {
+  min = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] != "number") continue;
+
+    if (array[i] < min) min = array[i];
+  }
+  return min;
+}
+
+function max(array) {
+  max = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] != "number") continue;
+
+    if (array[i] > max) max = array[i];
+  }
+  return max;
+}
+
+console.log(max(array3));
